ae923e5b446d82d532b44724bedd92412c9e2108
igastelu <igastelu@factoriamedia.com>
2022-05-02 01:45:21 -0500

    generador de presupuesto

--- generar-presupuesto/assets/js/main.js
+++ generar-presupuesto/assets/js/main.js
@@ -1,11 +1,11 @@
 (function ($) {
-  'use strict';
+  "use strict";
 
   /*--------------------------------------------------------------
   ## Down Load Button Function
   ----------------------------------------------------------------*/
-  $('#download_btn').on('click', function () {
-    var downloadSection = $('#download_section');
+  $("#download_btn").on("click", function () {
+    var downloadSection = $("#download_section");
     var cWidth = downloadSection.width();
     var cHeight = downloadSection.height();
     var topLeftMargin = 40;
@@ -18,12 +18,12 @@
     html2canvas(downloadSection[0], { allowTaint: true }).then(function (
       canvas
     ) {
-      canvas.getContext('2d');
-      var imgData = canvas.toDataURL('image/jpeg', 1.0);
-      var pdf = new jsPDF('p', 'pt', [pdfWidth, pdfHeight]);
+      canvas.getContext("2d");
+      var imgData = canvas.toDataURL("image/jpeg", 1.0);
+      var pdf = new jsPDF("p", "pt", [pdfWidth, pdfHeight]);
       pdf.addImage(
         imgData,
-        'JPG',
+        "JPG",
         topLeftMargin,
         topLeftMargin,
         canvasImageWidth,
@@ -33,16 +33,36 @@
         pdf.addPage(pdfWidth, pdfHeight);
         pdf.addImage(
           imgData,
-          'JPG',
+          "JPG",
           topLeftMargin,
           -(pdfHeight * i) + topLeftMargin * 0,
           canvasImageWidth,
           canvasImageHeight
         );
       }
-      pdf.save('ivonne-invoice.pdf');
+      pdf.save("ivonne-invoice.pdf");
     });
   });
+
+  $(".inputChange").change(function () {
+    var data_input = $(this).attr("data-id");
+    console.log(data_input);
+    $("#" + data_input).html($(this).val());
+  });
+
+  var number = 5000;
+  var percentX = 18;
+  var result;
+
+  function percentCalculation(a, b) {
+    var c = (parseFloat(a) * parseFloat(b)) / 100;
+    return parseFloat(c);
+  }
+
+  result = percentCalculation(number, percentX);
+  console.log(result);
+
+
 })(jQuery); // End of use strict
 
 // var editor = new EditorJS();
@@ -50,17 +70,17 @@
 // const editor = new EditorJS({
 //   holder: 'editorjs',
 //   tools: {
-    // image: SimpleImage,
-    // list: List,
-    // checklist: Checklist,
-    // quote: Quote,
-    // warning: Warning,
-    // marker: Marker,
-    // code: CodeTool,
-    // delimiter: Delimiter,
-    // inlineCode: InlineCode,
-    // linkTool: LinkTool,
-    // embed: Embed,
-    // table: Table
+// image: SimpleImage,
+// list: List,
+// checklist: Checklist,
+// quote: Quote,
+// warning: Warning,
+// marker: Marker,
+// code: CodeTool,
+// delimiter: Delimiter,
+// inlineCode: InlineCode,
+// linkTool: LinkTool,
+// embed: Embed,
+// table: Table
 //   },
 // });
