d5dfc1aafd194165fa12c704a7eb813163e9fdca
igastelu <igastelu@factoriamedia.com>
2022-05-04 11:02:40 -0500

    calculos

--- generar-presupuesto/assets/js/main.js
+++ generar-presupuesto/assets/js/main.js
@@ -40,13 +40,12 @@
           canvasImageHeight
         );
       }
-      pdf.save("ivonne-invoice.pdf");
+      pdf.save("ICONTSAC_presupuesto.pdf");
     });
   });
 
   $(".inputChange").change(function () {
     var data_input = $(this).attr("data-id");
-    console.log(data_input);
     $("#" + data_input).html($(this).val());
   });
 
@@ -59,10 +58,38 @@
     return parseFloat(c);
   }
 
-  result = percentCalculation(number, percentX);
-  console.log(result);
+  $(".calc_t").keyup(function () {
+    var sumParcGeneral = 0;
+    var data_input = $(this).attr("data-id");
+    $("#" + data_input).html($(this).val());
 
+    var dat_id = $(this).attr("data-sel");
+    var cant = $("#ct_cant_" + dat_id).val();
+    var precUnit = $("#ct_unit_" + dat_id).val();
+    var desct = $("#ct_desc_" + dat_id).val();
+    var sumPar = cant * precUnit - desct;
+    // calaculando el total parcial
+    $("#ct_tot_" + dat_id).val(sumPar);
+    for (var i = 1; i <= $(".sumTot").length; i++) {
+      $("#tot_parc_" + i).html($("#ct_tot_" + i).val());
+      sumParcGeneral += parseFloat($("#ct_tot_" + i).val());
+    }
+    $("#tot_parc_gen").val(sumParcGeneral);
+    $("#tot_parc").html(sumParcGeneral);
+    // calculado el igv
+    var porcent = parseFloat($("#valor_igv").val());
+    var cantiCalc = parseFloat($("#tot_parc_gen").val());
+    result = percentCalculation(cantiCalc, porcent);
+    console.log(result);
+    $("#calculo_igv").val(result);
+    $("#igv").html(result);
+    // mostrando el resultado
+    var igvFN = parseFloat($("#calculo_igv").val());
+    var TotPar = parseFloat($("#tot_parc_gen").val());
 
+    $("#totGenPres").val(TotPar + igvFN);
+    $("#tot_general").html(TotPar + igvFN);
+  });
 })(jQuery); // End of use strict
 
 // var editor = new EditorJS();
